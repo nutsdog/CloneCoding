7e1acfdcafc81e43390c31cf56ef8474474abec6
cjk <vmflsxj0@naver.com>
2022-04-17 09:01:51 +0900

    html, css, js로 만든 카카오페이지

---
+++ kakaopage/kakaopage/wwwroot/b_js/b-script.js
@@ -0,0 +1,49 @@
+﻿const slide = document.getElementsByClassName("slide")[0]
+const css = slide.style.cssText.toString();
+const slideItems = slide.getElementsByClassName("slide-items");
+let page = document.getElementsByClassName("css-1i9tz3i")[0];
+
+let count = 0;
+let timerId = 0;
+
+window.addEventListener("load", function () {
+    timerId = setTimeout(tick, 4000);
+});
+
+slide.addEventListener("mouseover", function () {
+    clearTimeout(timerId)
+});
+
+slide.addEventListener("mouseout", function () {
+    timerId = setTimeout(tick, 4000)
+});
+
+back.addEventListener("click", function () {
+    count--;
+
+    setPage();
+});
+
+next.addEventListener("click", function () {
+    count++;
+
+    setPage();
+});
+
+function tick() {
+    setPage();
+
+    count++;
+
+    timerId = setTimeout(tick, 4000);
+}
+
+function setPage() {
+    let length = count % slideItems.length;
+    const index = slideItems[length].getAttribute("data-index") * 720;
+    let replaceCssText = css.replace(/[(][0-9]*[p][x]/, "(" + -index + "px");
+
+    slide.style.cssText = replaceCssText;
+
+    page.innerHTML = (length + 1) + " / " + slideItems.length;
+}
